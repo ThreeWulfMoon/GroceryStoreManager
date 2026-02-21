45583d210ddf4a66b2b6854c3cf58f83625967bd
ThreeWulfMoon <thornmail1st@gmail.com>
2026-02-21 17:44:01 +0800

    Add project files.

---
+++ GroceryStoreManager/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace GroceryStoreManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            List<string> branch = new List<string>();
+
+        while (true) {
+                Console.WriteLine("\n Welcome to the Grocery Store Manager, User! \n");
+
+
+        }
+        static void 
+    }
+}
