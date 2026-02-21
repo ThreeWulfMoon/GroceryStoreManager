3901ff1684798bd3615b6b740fd131e433440445
ThreeWulfMoon <thornmail1st@gmail.com>
2026-02-21 18:22:05 +0800

    Finally completed Create option for now, might fix something later down the line

--- GroceryStoreManager/Program.cs
+++ GroceryStoreManager/Program.cs
@@ -13,17 +13,21 @@ namespace GroceryStoreManager
             branch.Add("ID: 67 | SupaMart Branch: Santo Tomas | Location: Binan, Laguna | Manager: Jotaro Kujo | Employees: 17");
             branch.Add("ID: 33 | SupaMart Branch: Pacita | Location: San Pedro, Laguna | Manager: David Martinez | Employees: 25");
 
-            while (true) {
-                Console.WriteLine("\n Welcome to the Grocery Store Manager, User! \n");
+            while (true)
+            {
+                Console.WriteLine("\n--- Welcome to the Grocery Store Manager, User! ---\n");
+                Console.WriteLine("");
+                Console.WriteLine("Please select the number of your choice: ");
+                Console.WriteLine("1. Establish a new Branch");
+                Console.WriteLine("2. View current Branches");
+                Console.WriteLine("3. Update Branch information");
+                Console.WriteLine("4. Delete a Branch");
 
-                Console.WriteLine("Establish a new Branch");
-                Console.WriteLine("View current Branches");
-                Console.WriteLine("Update Branch information");
-                Console.WriteLine("Delete a Branch");
-
+                Console.Write("Input Here: ");
                 choice = Console.ReadLine();
 
-                switch (choice) {
+                switch (choice)
+                {
                     case "1":
                         Create(branch);
                         break;
@@ -32,7 +36,8 @@ namespace GroceryStoreManager
 
             }
         }
-        static void Create(List<string> branch) {
+        static void Create(List<string> branch)
+        {
             Console.Write("Enter Branch ID");
             string branchID = Console.ReadLine();
             Console.Write("Enter the name of the new Branch: ");
@@ -43,5 +48,16 @@ namespace GroceryStoreManager
             string branchManager = Console.ReadLine();
             Console.Write("Enter the Number of Employees you want: ");
             string branchEmpl = Console.ReadLine();
+
+            string branchy = "ID: " + branchID + " | SupaMart Branch: " + branchName + " | Location: " + branchLoc + " | Manager: " + branchManager + " | Employees: " + branchEmpl;
+
+            branch.Add(branchy);
+
+            Console.WriteLine("New Branch Established!");
         }
+    }
+}
+
+
+
     
